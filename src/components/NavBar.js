7390ad83687c9815431e0253a419fb53605180f4
Joel <jatr160397@gmail.com>
2019-10-19 11:04:28 -0400

    se inicio la seccion de actividades

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,8 +11,8 @@ function NavBar(){
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#conocimientos">Conocimientos <span className="sr-only">(current)</span></a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#conocimientos">Skills</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#contacto">Contacto</a>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,15 @@
 import React from 'react'
+// Components
 import NavBar from '../components/NavBar'
 import Carrousel from '../components/Carrousel'
 import CardList from '../components/CardList'
 import Contacto from '../components/Contacto'
+import Activities from '../components/activities'
+
+// Data
+import CarrouselData from '../data/CarouselImageData.json'
 import ContactoData from '../data/ContactoData.json'
 import SkillsData from '../data/SkillsData.json'
-import CarrouselData from '../data/CarouselImageData.json'
-
 
 class Main extends React.Component {
     render() {
@@ -15,6 +18,7 @@ class Main extends React.Component {
                 <NavBar/> 
                 <Carrousel data={CarrouselData}/>
                 <CardList data={SkillsData}/>
+                <Activities/>
                 <Contacto data={ContactoData}/>
             </div>
         )
