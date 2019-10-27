1295fc8ffd72db96aea5c82dfee6b143512f9de7
Joel <jatr160397@gmail.com>
2019-10-27 10:14:43 -0400

    primeros pasos para agragar actividades

---
+++ src/components/activities.js
@@ -0,0 +1,18 @@
+import React from 'react'
+//import activity from './Activity'
+
+class Activities extends React.Component{
+    render(){
+        return(
+            <div className='Activities' id='Activities'>
+                <div className='container'>
+                    <h1>Aqui ira el listado de actividades</h1>
+                
+                </div>
+            
+            </div>
+        )
+    }
+}
+
+export default Activities
